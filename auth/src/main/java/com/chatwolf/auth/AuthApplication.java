56603e36ee54ec3357126c087a17d37163d53071
Nitesh Yadav <nteshxx@gmail.com>
2025-11-04 13:04:39 +0530

    removed redis commander and added redisinsight

--- auth/src/main/java/com/chatwolf/auth/AuthApplication.java
+++ auth/src/main/java/com/chatwolf/auth/AuthApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
