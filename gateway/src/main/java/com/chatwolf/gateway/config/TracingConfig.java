98f2aa8e964baca7ee2d51fba80bfe6892211b91
Nitesh Yadav <nteshxx@gmail.com>
2025-11-12 01:00:16 +0530

    added TracingConfig to ignore actuator and security spans

---
+++ gateway/src/main/java/com/chatwolf/gateway/config/TracingConfig.java
@@ -0,0 +1,39 @@
+package com.chatwolf.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.observation.SecurityObservationSettings;
+
+import io.micrometer.tracing.exporter.SpanExportingPredicate;
+
+@Configuration
+public class TracingConfig {
+	
+	@Bean
+    SecurityObservationSettings noSpringSecurityObservations() {
+    	return SecurityObservationSettings.noObservations();
+    }
+
+    @Bean
+    SpanExportingPredicate noActuatorObservations() {
+        return span -> {
+            String uri = span.getTags().get("uri");
+            return uri == null || !uri.startsWith("/actuator");
+        };
+    }
+
+    @Bean
+    SpanExportingPredicate noEurekaClientObservations() {
+        return span -> {
+            String clientName = span.getTags().get("client.name");
+
+            // Early return for most common case
+            if ("eureka".equals(clientName)) {
+                return false;
+            }
+
+            String httpUrl = span.getTags().get("http.url");
+            return httpUrl == null || !httpUrl.contains("/eureka/");
+        };
+    }
+}
